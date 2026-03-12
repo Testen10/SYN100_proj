b7fda96282927b798f13cbf4edbfdf61591e9628
Testen10 <minseochu10@gmail.com>
2026-03-11 17:29:41 -0700

    add content

--- assets/js/whereJs.js
+++ assets/js/whereJs.js
@@ -14,7 +14,7 @@ const mapUrls = [
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3350.4600705540647!2d-117.24607022390416!3d32.88600227361832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc0797fd5dfae7%3A0x1168f9ed95dbe025!2sEllies%20Garden!5e0!3m2!1sen!2sus!4v1773098786611!5m2!1sen!2sus", // loc12
     "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d942.3497055910183!2d-117.2336944!3d32.882916699999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzLCsDUyJzU4LjUiTiAxMTfCsDE0JzAxLjMiVw!5e1!3m2!1sen!2sus!4v1773099078776!5m2!1sen!2sus", // loc13
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.196485723533!2d-117.24685181356996!3d32.887673605592525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc06eb12db88c5%3A0xa96d604c0343144b!2sSeventh%20Market!5e1!3m2!1sen!2sus!4v1773101682393!5m2!1sen!2sus", // loc14
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.574209733745!2d-117.24522142390443!3d32.87879287362127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc00d44855dd09%3A0x573f410b752e332b!2sMOM&#39;s%20Caf%C3%A9%20(aka%20Muir%20Woods%20Coffee%20House)!5e1!3m2!1sen!2sus!4v1773102478742!5m2!1sen!2sus", // loc15
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.574209733745!2d-117.24522142390443!3d32.87879287362127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc00d44855dd09%3A0x573f410b752e332b!2sMOMs%20Cafe%20(aka%20Muir%20Woods%20Coffee%20House)!5e1!3m2!1sen!2sus!4v1773102478742!5m2!1sen!2sus", // loc15
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3350.885609487068!2d-117.24460902390459!3d32.87474587362303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc06c7b8154acf%3A0x51e8bb9f09bfd68!2s64%20Degrees!5e0!3m2!1sen!2sus!4v1773097167389!5m2!1sen!2sus", // loc16
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.351722160637!2d-117.24474492390425!3d32.88402407361913!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc06ea9cbc1b3f%3A0xa8ef76ba49a3930b!2sInternational%20House!5e1!3m2!1sen!2sus!4v1773103024599!5m2!1sen!2sus", // loc17
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7539.438854507512!2d-117.24442722390457!3d32.87537807362275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc06c62b85cfb3%3A0xb5a1c2bd95eb492e!2sRevelle%20College!5e1!3m2!1sen!2sus!4v1773103065422!5m2!1sen!2sus", // loc18
@@ -23,20 +23,19 @@ const mapUrls = [
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.4930559367417!2d-117.24462042390445!3d32.880701073620465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc06db2fd75861%3A0x74cff9530abaf1f3!2sSixth%20College!5e1!3m2!1sen!2sus!4v1773103392187!5m2!1sen!2sus", // loc21
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.4560181862403!2d-117.23665632390446!3d32.88157192362015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc06c368a29faf%3A0x421446af03678d9c!2sEarl%20Warren%20College!5e1!3m2!1sen!2sus!4v1773103449855!5m2!1sen!2sus", // loc22
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.180891186235!2d-117.24741471356975!3d32.88804020559197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc07ecf9dab909%3A0x5cf33bc80a4bd726!2sSeventh%20College!5e1!3m2!1sen!2sus!4v1773103492292!5m2!1sen!2sus", // loc23
-    "", // loc24
-    "", // loc25
-    "", // loc26
-    "", // loc27
-    "", // loc28
-    "", // loc29
-    "", // loc30
-    "", // loc31
-    "", // loc32
-    "", // loc33
-    "", // loc34
-    "", // loc35
-    "", // loc36
-    "", // loc37
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.834586436066!2d-117.24503462390473!3d32.87266987362399!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc07e85ca64a51%3A0x86d4382a17dafab1!2sEighth%20College!5e1!3m2!1sen!2sus!4v1773104482326!5m2!1sen!2sus", // loc24
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.6254847676996!2d-117.2331891239046!3d32.87758717362194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc06dae739e26f%3A0xcd6cb0604149a9d4!2sPepper%20Canyon%20East%20Apartments!5e1!3m2!1sen!2sus!4v1773104550048!5m2!1sen!2sus", // loc25
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.824887399876!2d-117.23788162390474!3d32.872897973623786!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc06cfaa5f3321%3A0x6cfba2f5017b93ff!2sRita%20Atkinson%20Residences!5e1!3m2!1sen!2sus!4v1773104590775!5m2!1sen!2sus", // loc26
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.597241306555!2d-117.23806491357219!3d32.87825130560655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc07000bdf288d%3A0x4d81a81cb3c8aafe!2sPepper%20Canyon%20West%20(Rya)!5e1!3m2!1sen!2sus!4v1773104646189!5m2!1sen!2sus", // loc27
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3516.5383720113377!2d-82.42939732368609!3d38.42271197182923!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88460833de0e3443%3A0xe4572a8990f87d9b!2sMarshall%20University%20Housing%20and%20Residence%20Life!5e1!3m2!1sen!2sus!4v1773118967642!5m2!1sen!2sus", // loc28
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d471.2134820061447!2d-117.22274556737236!3d32.8756501828426!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc06d7b5d0b667%3A0x372d8d464425b1f0!2sMesa%20Nueva%20-%20Cala!5e1!3m2!1sen!2sus!4v1773250338701!5m2!1sen!2sus", // loc29
+    "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d925.1670895258569!2d-117.22351210408252!3d32.87516799231661!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sus!4v1773251026871!5m2!1sen!2sus", // loc30
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d491.622152057004!2d-117.22226510245957!3d32.87208701523473!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc06d5dd1f0b77%3A0xdfb2d32c5935f2a8!2s9156%20Regents%20Rd%2C%20La%20Jolla%2C%20CA%2092037!5e1!3m2!1sen!2sus!4v1773251517265!5m2!1sen!2sus", // loc31
+    "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d471.23246278424193!2d-117.2213660273157!3d32.87207919782463!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sus!4v1773251609434!5m2!1sen!2sus", // loc32
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d240.9664368286003!2d-117.22159355075979!3d32.87344493832162!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc06d60fccd75f%3A0xfdd08b56013cfc1a!2s9238%20Regents%20Rd%2C%20La%20Jolla%2C%20CA%2092037!5e1!3m2!1sen!2sus!4v1773251689885!5m2!1sen!2sus", // loc33
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d383.0353050842697!2d-117.21973045290912!3d32.872980546731235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc072a1cff9cb9%3A0x492d17f3faa356e0!2s9264%2C%20La%20Jolla%2C%20CA%2092037!5e1!3m2!1sen!2sus!4v1773251878921!5m2!1sen!2sus", // loc34
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d446.53278988498226!2d-117.22151854100193!3d32.865626025356164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc012a84d575ef%3A0xacc05dec36ce7079!2s8186%20Regents%20Rd%2C%20San%20Diego%2C%20CA%2092122!5e1!3m2!1sen!2sus!4v1773251997218!5m2!1sen!2sus", // loc35
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d471.2454346655824!2d-117.246628999491!3d32.86963850997429!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc06b7aaf6ff1b%3A0xbf519f23b0fac291!2s9334%20Redwood%20Dr%2C%20La%20Jolla%2C%20CA%2092037!5e1!3m2!1sen!2sus!4v1773252083536!5m2!1sen!2sus", // loc36
 ];
 
 // DOM이 로드된 후 이벤트 리스너 추가
@@ -55,7 +54,15 @@ document.addEventListener('DOMContentLoaded', function() {
     
     // map radio buttons and Iframe
     const mapIframe = document.getElementById('mapIframe');
-    const mapOverlay = document.getElementById('mapOverlay');
+    const mapOverlay1 = document.getElementById('mapOverlay1');
+    const mapOverlay2 = document.getElementById('mapOverlay2');
+    const mapOverlay3 = document.getElementById('mapOverlay3');
+    const mapOverlay4 = document.getElementById('mapOverlay4');
+    const mapOverlay5 = document.getElementById('mapOverlay5');
+    const mapOverlay6 = document.getElementById('mapOverlay6');
+    const mapOverlay7 = document.getElementById('mapOverlay7');
+    const mapOverlay8 = document.getElementById('mapOverlay8');
+    const mapOverlay9 = document.getElementById('mapOverlay9');
 
     // list transition
     type1Radio.addEventListener('change', function(){
@@ -65,7 +72,15 @@ document.addEventListener('DOMContentLoaded', function() {
             type3_list.style.display="none";
             type4_list.style.display="none";
             type5_list.style.display="none";
-            if (mapOverlay) mapOverlay.style.display = "none";
+
+            mapOverlay2.style.display = "none";
+            mapOverlay3.style.display = "none";
+            mapOverlay4.style.display = "none";
+            mapOverlay5.style.display = "none";
+            mapOverlay6.style.display = "none";
+            mapOverlay7.style.display = "none";
+            mapOverlay8.style.display = "none";
+            if (mapOverlay1) mapOverlay1.style.display = "none";
         }
     });
     type2Radio.addEventListener('change', function(){
@@ -75,7 +90,15 @@ document.addEventListener('DOMContentLoaded', function() {
             type3_list.style.display="none";
             type4_list.style.display="none";
             type5_list.style.display="none";
-            if (mapOverlay) mapOverlay.style.display = "block";
+
+            mapOverlay2.style.display = "none";
+            mapOverlay3.style.display = "none";
+            mapOverlay4.style.display = "none";
+            mapOverlay5.style.display = "none";
+            mapOverlay6.style.display = "none";
+            mapOverlay7.style.display = "none";
+            mapOverlay8.style.display = "none";
+            if (mapOverlay1) mapOverlay1.style.display = "block";
         }
     });
     type3Radio.addEventListener('change', function(){
@@ -85,7 +108,15 @@ document.addEventListener('DOMContentLoaded', function() {
             type3_list.style.display="flex";
             type4_list.style.display="none";
             type5_list.style.display="none";
-            if (mapOverlay) mapOverlay.style.display = "none";
+
+            mapOverlay2.style.display = "none";
+            mapOverlay3.style.display = "none";
+            mapOverlay4.style.display = "none";
+            mapOverlay5.style.display = "none";
+            mapOverlay6.style.display = "none";
+            mapOverlay7.style.display = "none";
+            mapOverlay8.style.display = "none";
+            if (mapOverlay1) mapOverlay1.style.display = "none";
         }
     });
     type4Radio.addEventListener('change', function(){
@@ -95,7 +126,15 @@ document.addEventListener('DOMContentLoaded', function() {
             type3_list.style.display="none";
             type4_list.style.display="flex";
             type5_list.style.display="none";
-            if (mapOverlay) mapOverlay.style.display = "none";
+
+            mapOverlay2.style.display = "none";
+            mapOverlay3.style.display = "none";
+            mapOverlay4.style.display = "none";
+            mapOverlay5.style.display = "none";
+            mapOverlay6.style.display = "none";
+            mapOverlay7.style.display = "none";
+            mapOverlay8.style.display = "none";
+            if (mapOverlay1) mapOverlay1.style.display = "none";
         }
     });
     type5Radio.addEventListener('change', function(){
@@ -105,7 +144,15 @@ document.addEventListener('DOMContentLoaded', function() {
             type3_list.style.display="none";
             type4_list.style.display="none";
             type5_list.style.display="flex";
-            if (mapOverlay) mapOverlay.style.display = "none";
+
+            mapOverlay2.style.display = "none";
+            mapOverlay3.style.display = "none";
+            mapOverlay4.style.display = "none";
+            mapOverlay5.style.display = "none";
+            mapOverlay6.style.display = "none";
+            mapOverlay7.style.display = "none";
+            mapOverlay8.style.display = "none";
+            if (mapOverlay1) mapOverlay1.style.display = "none";
         }
     });
 
@@ -116,6 +163,95 @@ document.addEventListener('DOMContentLoaded', function() {
             locRadio.addEventListener('change', function() {
                 if (this.checked) {
                     mapIframe.src = mapUrls[i-1];
+                    if(i==29){ // mesa nueava green waste dumpster
+                        mapOverlay2.style.display = "block";
+                        mapOverlay3.style.display = "none";
+                        mapOverlay4.style.display = "none";
+                        mapOverlay5.style.display = "none";
+                        mapOverlay6.style.display = "none";
+                        mapOverlay7.style.display = "none";
+                        mapOverlay8.style.display = "none";
+                        mapOverlay9.style.display = "none";
+                    }
+                    else if (i==30){
+                        mapOverlay2.style.display = "none";
+                        mapOverlay3.style.display = "block";
+                        mapOverlay4.style.display = "none";
+                        mapOverlay5.style.display = "none";
+                        mapOverlay6.style.display = "none";
+                        mapOverlay7.style.display = "none";
+                        mapOverlay8.style.display = "none";
+                        mapOverlay9.style.display = "none";
+                    }
+                    else if (i==31){
+                        mapOverlay2.style.display = "none";
+                        mapOverlay3.style.display = "none";
+                        mapOverlay4.style.display = "block";
+                        mapOverlay5.style.display = "none";
+                        mapOverlay6.style.display = "none";
+                        mapOverlay7.style.display = "none";
+                        mapOverlay8.style.display = "none";
+                        mapOverlay9.style.display = "none";
+                    }
+                    else if (i==32){
+                        mapOverlay2.style.display = "none";
+                        mapOverlay3.style.display = "none";
+                        mapOverlay4.style.display = "none";
+                        mapOverlay5.style.display = "block";
+                        mapOverlay6.style.display = "none";
+                        mapOverlay7.style.display = "none";
+                        mapOverlay8.style.display = "none";
+                        mapOverlay9.style.display = "none";
+                    }
+                    else if (i==33){
+                        mapOverlay2.style.display = "none";
+                        mapOverlay3.style.display = "none";
+                        mapOverlay4.style.display = "none";
+                        mapOverlay5.style.display = "none";
+                        mapOverlay6.style.display = "block";
+                        mapOverlay7.style.display = "none";
+                        mapOverlay8.style.display = "none";
+                        mapOverlay9.style.display = "none";
+                    }
+                    else if (i==34){
+                        mapOverlay2.style.display = "none";
+                        mapOverlay3.style.display = "none";
+                        mapOverlay4.style.display = "none";
+                        mapOverlay5.style.display = "none";
+                        mapOverlay6.style.display = "none";
+                        mapOverlay7.style.display = "block";
+                        mapOverlay8.style.display = "none";
+                        mapOverlay9.style.display = "none";
+                    }
+                    else if (i==35){
+                        mapOverlay2.style.display = "none";
+                        mapOverlay3.style.display = "none";
+                        mapOverlay4.style.display = "none";
+                        mapOverlay5.style.display = "none";
+                        mapOverlay6.style.display = "none";
+                        mapOverlay7.style.display = "none";
+                        mapOverlay8.style.display = "block";
+                        mapOverlay9.style.display = "none";
+                    }
+                    else if (i==36){
+                        mapOverlay2.style.display = "none";
+                        mapOverlay3.style.display = "none";
+                        mapOverlay4.style.display = "none";
+                        mapOverlay5.style.display = "none";
+                        mapOverlay6.style.display = "none";
+                        mapOverlay7.style.display = "none";
+                        mapOverlay8.style.display = "none";
+                        mapOverlay9.style.display = "block";
+                    }
+                    else{
+                        mapOverlay2.style.display = "none";
+                        mapOverlay3.style.display = "none";
+                        mapOverlay4.style.display = "none";
+                        mapOverlay5.style.display = "none";
+                        mapOverlay6.style.display = "none";
+                        mapOverlay7.style.display = "none";
+                        mapOverlay8.style.display = "none";
+                    }
                 }
             });
         }
